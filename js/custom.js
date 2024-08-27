6a9501bccfa6744e5f500444b59e8a41629763ee
党刘锋 <2539772207@qq.com>
2024-08-27 11:16:06 +0800

    Site updated: 2024-08-27 11:16:05

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// custom.js
+(function() {
+    // Google AdSense Script
+    var adScript = document.createElement("script");
+    adScript.async = true;
+    adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3929357066669626";
+    adScript.crossorigin = "anonymous";
+    document.head.appendChild(adScript);
+})();
